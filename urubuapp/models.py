34688a158528331be7b61e48f5dd47872a14dbe7
Narciso Junior <boldrin.narciso@gmail.com>
2023-10-04 00:03:32 +0100

    add db table

--- urubuapp/models.py
+++ urubuapp/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-import datetime
-from django.utils import timezone
 
-class User(models.Model):
+class UserModel(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
     
-class Wallet(models.Model):
+    class Meta:
+        db_table = "user"
+    
+class WalletModel(models.Model):
     amount = models.CharField(max_length=30)
     owner = models.ForeignKey("User", on_delete=models.CASCADE,)
     last_updated = models.DateTimeField("last updated")
 
     def last_transaction(self):
         return self.last_updated
+    
+    class Meta:
+        db_table = "wallet"
